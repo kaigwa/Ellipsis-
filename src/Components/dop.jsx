eff2e96cca3196ba95c47806fcb4fb6f2da03f07
Aloycius Millinga <45605465+aloycius@users.noreply.github.com>
2022-10-18 16:13:48 +0300

    responsive changes

--- src/Components/dop.jsx
+++ src/Components/dop.jsx
@@ -1,121 +1,156 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import {ImageBackground, View} from 'react';
 import { useNavigate } from "react-router-dom";
-import './dop/dop.css';
+import "./dop/dop.css";
 import { Link } from "react-router-dom";
-import Bootcamp from '../assets/Bootcamp.jpg'; // gives image path
+import Bootcamp from "../assets/Bootcamp.jpg"; // gives image path
 // import Scrollspy from 'react-scrollspy'
-import ScrollSpy from 'react-scrollspy';
+import ScrollSpy from "react-scrollspy";
 
 const Dop = (props) => {
-    const navigate = useNavigate(); 
-    return (
-      
-      <>
-      <div className='dp' style ={ {backgroundImage: "url('./images/dope.jpg')"}}>
-        <nav className='nav'>
-          <div className='logo'>
-          <a className='remove-link' href="https://ellipsis.co.tz"><h1><span className='neno'>DOP</span><span><img className='nembo' src="./images/Ellipsis logo 2.png"/></span><span className='e'>E</span></h1></a>
+  const navigate = useNavigate();
+  return (
+    <>
+      <div
+        className="dp"
+        style={{ backgroundImage: "url('./images/dope.jpg')" }}
+      >
+        <nav className="nav">
+          <div className="logo">
+            <a className="remove-link" href="https://ellipsis.co.tz">
+              <h1>
+                <span className="neno">DOP</span>
+                <span>
+                  <img className="nembo" src="./images/Ellipsis logo 2.png" />
+                </span>
+                <span className="e">E</span>
+              </h1>
+            </a>
           </div>
           {/* <Scrollspy items={ ['#about-dope','#courses'] } currentClassName="is-current"> */}
-            <ul>
-              <div className='dropdown'>
-                <li><button className='bar'>About</button></li>
-                
-                <div className='dropdown-content'>
-                    <a href="#about-dope" ref={React.createRef()}>About DOP@E</a>
-                    <a href="">Why DOP@E</a>
-                    <a href="">Achievements</a>
-                </div>
-              </div>
-              
-              
-              <div className='dropdown'>
-                <li><button className='bar'>Programs</button></li>
-                <div className='dropdown-content'>
-                    <a href="#courses" ref={React.createRef()}>Courses</a>
-                    <a href="">Schedule</a>
-                </div>
-              </div>
-             
-             <div className='dropdown'>
-              <li><button className='bar'>Initiators</button></li>
-             </div>
-              
-              <div className='dropdown'>
-                <li><button className='bar'>Contact Us</button></li>
-              </div>
+          <ul>
+            <div className="dropdown">
+              <li>
+                <button className="bar">About</button>
+              </li>
 
-            </ul>
-            {/* </Scrollspy> */}
-          
+              <div className="dropdown-content">
+                <a href="#about-dope" ref={React.createRef()}>
+                  About DOP@E
+                </a>
+                <a href="">Why DOP@E</a>
+                <a href="">Achievements</a>
+              </div>
+            </div>
+
+            <div className="dropdown">
+              <li>
+                <button className="bar">Programs</button>
+              </li>
+              <div className="dropdown-content">
+                <a href="#courses" ref={React.createRef()}>
+                  Courses
+                </a>
+                <a href="">Schedule</a>
+              </div>
+            </div>
+
+            <div className="dropdown">
+              <li>
+                <button className="bar">Initiators</button>
+              </li>
+            </div>
+
+            <div className="dropdown">
+              <li>
+                <button className="bar">Contact Us</button>
+              </li>
+            </div>
+          </ul>
+          {/* </Scrollspy> */}
         </nav>
-        
-        <div className='dope'>
-            <p className='para'><strong className='dev'>Developers </strong><br /><strong className='reach'>Outreach Program </strong><br /><span className='code'>The Best Coding Training Program In</span><br /><span className='tz'>Tanzania</span></p>
-      
-            <div className='an'>
-                <button className='btn3' onClick={() => navigate("/Form")}>APPLY NOW!</button>
-            </div>
-            
+
+        <div className="dope">
+          <p className="para">
+            <strong className="dev">Developers </strong>
+            <br />
+            <strong className="reach">Outreach Program </strong>
+            <br />
+            <span className="code">The Best Coding Training Program In</span>
+            <br />
+            <span className="tz">Tanzania</span>
+          </p>
+
+          <div className="an">
+            <button className="btn3" onClick={() => navigate("/Form")}>
+              APPLY NOW!
+            </button>
           </div>
-           <div className='outercontainer'>
-
-           <div className='cardcontainer'>
-            <div className='card' id='about-dope'>
-            <h4><b>ABOUT DOPE</b></h4>
+        </div>
+        <div className="outercontainer">
+          <div className="cardcontainer">
+            <div className="card" id="about-dope">
+              <h4>
+                <b>ABOUT DOPE</b>
+              </h4>
             </div>
-           </div>
-           
-           <div className='cardcontainer'>
-            <div className='card'>
-            <h4><b>WHY DOP@E?</b></h4>
-            </div>
-           </div>
-
-            <div className='cardcontainer'>
-            <div className='card'>
-            <h4><b>ACHIEVEMENTS</b></h4>
-            </div>
-           </div>
-
-          </div>
-          <div className='header'>
-             <h2>PROGRAMS</h2><br/>
-             </div>
-          <div className='outercontainer'>
-            
-           <div className='cardcontainer'>
-            <div className='card'>
-            <h4><b>ABOUT DOPE</b></h4>
-            </div>
-           </div>
-           
-           <div className='cardcontainer'>
-            <div className='card' id='courses'>
-            <h4><b>WHY DOP@E?</b></h4>
-            </div>
-           </div>
-
-            <div className='cardcontainer'>
-            <div className='card'>
-            <h4><b>ACHIEVEMENTS</b></h4>
-            </div>
-           </div>
-
           </div>
 
-           <div className='grid-container'>
-             <div className='grid-item'><img src={Bootcamp} alt="this is car image" /></div>
-             <div className='grid-item'>2</div>
+          <div className="cardcontainer">
+            <div className="card">
+              <h4>
+                <b>WHY DOP@E?</b>
+              </h4>
+            </div>
           </div>
-          
+
+          <div className="cardcontainer">
+            <div className="card">
+              <h4>
+                <b>ACHIEVEMENTS</b>
+              </h4>
+            </div>
+          </div>
+        </div>
+        <div className="header">
+          <h2>PROGRAMS</h2>
+          <br />
+        </div>
+        <div className="outercontainer">
+          <div className="cardcontainer">
+            <div className="card">
+              <h4>
+                <b>ABOUT DOPE</b>
+              </h4>
+            </div>
+          </div>
+
+          <div className="cardcontainer">
+            <div className="card" id="courses">
+              <h4>
+                <b>WHY DOP@E?</b>
+              </h4>
+            </div>
+          </div>
+
+          <div className="cardcontainer">
+            <div className="card">
+              <h4>
+                <b>ACHIEVEMENTS</b>
+              </h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-container">
+          <div className="grid-item">
+            <img src={Bootcamp} alt="this is car image" />
+          </div>
+          <div className="grid-item">2</div>
+        </div>
       </div>
-      </>
+    </>
+  );
+};
 
-    )
-  
-  }
- 
 export default Dop;
-
