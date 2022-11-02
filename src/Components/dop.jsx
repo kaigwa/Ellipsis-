161779d7bfde7b992d04664bfad8dcbf33d57c5d
Aloycius Millinga <45605465+aloycius@users.noreply.github.com>
2022-11-02 17:35:07 +0300

    responsive

--- src/Components/dop.jsx
+++ src/Components/dop.jsx
@@ -11,81 +11,84 @@ const Dop = (props) => {
   const navigate = useNavigate();
   return (
     <>
-      <div
-        className="dp"
-        style={{ backgroundImage: "url('./images/dope.jpg')" }}
-      >
-        <div className="logo">
-          <a className="remove-link" href="https://ellipsis.co.tz">
-            <h1>
-              <span className="neno">DOP</span>
-              <span>
-                <img className="nembo" src="./images/Ellipsis logo 2.png" />
-              </span>
-              <span className="e">E</span>
-            </h1>
-          </a>
-        </div>
-        <nav className="nav">
-          {/* <Scrollspy items={ ['#about-dope','#courses'] } currentClassName="is-current"> */}
-          <ul>
-            <div className="dropdown">
-              <li>
-                <button className="bar">About</button>
-              </li>
+      <div className="main-container">
+        <div
+          className="dp"
+          style={{ backgroundImage: "url('./images/dope.jpg')" }}
+        >
+          <div className="logo">
+            <a className="remove-link" href="https://ellipsis.co.tz">
+              <h1>
+                <span className="neno">DOP</span>
+                <span>
+                  <img className="nembo" src="./images/Ellipsis logo 2.png" />
+                </span>
+                <span className="e">E</span>
+              </h1>
+            </a>
+          </div>
+          <nav className="nav">
+            {/* <Scrollspy items={ ['#about-dope','#courses'] } currentClassName="is-current"> */}
+            <ul>
+              <div className="dropdown">
+                <li>
+                  <button className="bar">About</button>
+                </li>
 
-              <div className="dropdown-content">
-                <a href="#about-dope" ref={React.createRef()}>
-                  About DOP@E
-                </a>
-                <a href="">Why DOP@E</a>
-                <a href="">Achievements</a>
+                <div className="dropdown-content">
+                  <a href="#about-dope" ref={React.createRef()}>
+                    About DOP@E
+                  </a>
+                  <a href="">Why DOP@E</a>
+                  <a href="">Achievements</a>
+                </div>
               </div>
-            </div>
 
-            <div className="dropdown">
-              <li>
-                <button className="bar">Programs</button>
-              </li>
-              <div className="dropdown-content">
-                <a href="#courses" ref={React.createRef()}>
-                  Courses
-                </a>
-                <a href="">Schedule</a>
+              <div className="dropdown">
+                <li>
+                  <button className="bar">Programs</button>
+                </li>
+                <div className="dropdown-content">
+                  <a href="#courses" ref={React.createRef()}>
+                    Courses
+                  </a>
+                  <a href="">Schedule</a>
+                </div>
               </div>
-            </div>
 
-            <div className="dropdown">
-              <li>
-                <button className="bar">Initiators</button>
-              </li>
-            </div>
+              <div className="dropdown">
+                <li>
+                  <button className="bar">Initiators</button>
+                </li>
+              </div>
 
-            <div className="dropdown">
-              <li>
-                <button className="bar">Contact Us</button>
-              </li>
-            </div>
-          </ul>
-          {/* </Scrollspy> */}
-        </nav>
+              <div className="dropdown">
+                <li>
+                  <button className="bar">Contact Us</button>
+                </li>
+              </div>
+            </ul>
+            {/* </Scrollspy> */}
+          </nav>
 
-        <div className="dope">
-          <p className="para">
-            <strong className="dev">Developers </strong>
-            <br />
-            <strong className="reach">Outreach Program </strong>
-            <br />
-            <span className="code">The Best Coding Training Program In</span>
-            <br />
-            <span className="tz">Tanzania</span>
-          </p>
-          <div className="an">
-            <button className="btn3" onClick={() => navigate("/Form")}>
-              APPLY NOW!
-            </button>
+          <div className="dope">
+            <p className="para">
+              <strong className="dev">Developers </strong>
+              <br />
+              <strong className="reach">Outreach Program </strong>
+              <br />
+              <span className="code">The Best Coding Training Program In</span>
+              <br />
+              <span className="tz">Tanzania</span>
+            </p>
+            <div className="an">
+              <button className="btn3" onClick={() => navigate("/Form")}>
+                APPLY NOW!
+              </button>
+            </div>
           </div>
         </div>
+
         <div className="outercontainer">
           <div className="cardcontainer">
             <div className="card" id="about-dope">
